91a82d0622f283eccd5b210ea28520b9f5a46bae
olegbaslak <oleg.baslak@gmail.com>
2016-10-03 11:48:31 +0300

    lesson1.task05 init structure

---
+++ src/com/rct/homework/lesson1/task05/Main.java
@@ -0,0 +1,18 @@
+package com.rct.homework.lesson1.task05;
+
+/**
+ * Entrance point to the app
+ *
+ * @author Oleg Baslak
+ * @version 1.0
+ * @since 03-10-2016
+ */
+public class Main {
+
+    /**
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/rct/homework/lesson1/task05/Triangle.java
@@ -0,0 +1,10 @@
+package com.rct.homework.lesson1.task05;
+
+/**
+ * @author Oleg Baslak
+ * @version 1.0
+ * @since 03-10-2016
+ */
+public class Triangle {
+
+}
