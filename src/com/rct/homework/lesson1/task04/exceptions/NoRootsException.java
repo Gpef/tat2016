5c719596047fc788b8adb68b0d4e3151042f183e
olegbaslak <oleg.baslak@gmail.com>
2016-10-04 17:19:27 +0300

    lesson1task04 added doc to NoRootsException class

--- src/com/rct/homework/lesson1/task04/exceptions/NoRootsException.java
+++ src/com/rct/homework/lesson1/task04/exceptions/NoRootsException.java
@@ -1,12 +1,15 @@
 package com.rct.homework.lesson1.task04.exceptions;
 
 /**
+ * Exception thrown when equation doesn't have any roots
+ * or there no method to find them
+ *
  * @author Oleg Baslak
  * @version 1.0
  * @since 04-10-2016
  */
 public class NoRootsException extends Exception {
-    public NoRootsException(){
+    public NoRootsException() {
         super("Equation don't has any roots");
     }
 
