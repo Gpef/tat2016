27bc27de508bde9e30e7c3f7f5639b6ec08123e2
olegb <oleg.baslak@gmail.com>
2016-10-08 16:52:18 +0300

    lesson2task08 words validator done

--- src/com/rct/homework/lesson2/task07/Main.java
+++ src/com/rct/homework/lesson2/task07/Main.java
@@ -1,7 +1,14 @@
 package com.rct.homework.lesson2.task07;
 
+import com.rct.homework.lesson2.task07.localisation.MessageStrings;
+import com.rct.homework.lesson2.task07.validator.*;
+
+import java.util.ArrayList;
+
 /**
- * Entrance point to the program. It's a main class
+ * Entrance point to the program.
+ * Gets inputted string and validates
+ * for several rules.
  *
  * @author Oleg Baslak
  * @version 1.0
@@ -10,11 +17,35 @@ package com.rct.homework.lesson2.task07;
 public class Main {
 
     /**
-     * Entrance point to the program
+     * Entrance point to the program.
      *
      * @param args command line arguments
      */
     public static void main(String[] args) {
+        System.out.println(MessageStrings.HELLO_MESSAGE);
+        //String stringToValidate = InputController.getInput();
+        String stringToValidate = "O1leg a a Java Java a";
+        System.out.println(stringToValidate);
+        ArrayList<Validator> validators = initValidators();
+        for (Validator validator : validators
+                ) {
+            if (validator.validate(stringToValidate)) {
+                System.out.println(validator.getPassMessage());
+            }
+        }
+    }
 
+    /**
+     * Initialises validators to perform checks.
+     *
+     * @return initialised <code>ArrayList</code> of validators
+     */
+    private static ArrayList<Validator> initValidators() {
+        ArrayList<Validator> validators = new ArrayList<>();
+        validators.add(new DictionaryValidator());
+        validators.add(new NoNumberValidator());
+        validators.add(new OnlyNumberValidator());
+        validators.add(new WordsValidator());
+        return validators;
     }
 }
