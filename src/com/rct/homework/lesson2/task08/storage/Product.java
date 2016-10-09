f81f23f621f15515bda9c5473d0cb8144b04e514
olegb <oleg.baslak@gmail.com>
2016-10-10 01:09:52 +0300

    Product class done

--- src/com/rct/homework/lesson2/task08/storage/Product.java
+++ src/com/rct/homework/lesson2/task08/storage/Product.java
@@ -1,9 +1,14 @@
 package com.rct.homework.lesson2.task08.storage;
 
+import com.rct.homework.lesson2.task08.exceptions.PriceParseException;
+
 import java.math.BigDecimal;
 
 /**
  * Class that represents product at the storage.
+ * If user inputs empty string "" as type or product
+ * name, they will be manually changed to "no type" and
+ * "no name".
  *
  * @author Oleg Baslak
  * @version 1.0
@@ -11,15 +16,28 @@ import java.math.BigDecimal;
  */
 public class Product {
 
+    private static final String defaultType = "no type";
+    private static final String defaultName = "no name";
+
     private String type;
     private String name;
     private BigDecimal price;
 
-    public Product(String _type, String _name, String _price){
+    public Product(String _type, String _name, String _price) throws PriceParseException {
+        try {
+            price = new BigDecimal(_price);
+            price = price.setScale(2, BigDecimal.ROUND_HALF_UP);
+        } catch (NumberFormatException e) {
+            throw new PriceParseException();
+        }
+        if ("".equals(_type)) {
+            _type = defaultType;
+        }
+        if ("".equals(_name)) {
+            _name = defaultName;
+        }
         type = _type;
         name = _name;
-        price = new BigDecimal(_price);
-        price = price.setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
     public String getType() {
