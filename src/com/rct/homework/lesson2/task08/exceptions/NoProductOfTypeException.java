d93503c23aac41c9a209a51807ca7a88e397abff
olegb <oleg.baslak@gmail.com>
2016-10-10 00:00:03 +0300

    exceptions added

--- src/com/rct/homework/lesson2/task08/exceptions/NoProductOfTypeException.java
+++ src/com/rct/homework/lesson2/task08/exceptions/NoProductOfTypeException.java
@@ -8,9 +8,9 @@ package com.rct.homework.lesson2.task08.exceptions;
  * @version 1.0
  * @since 08.10.2016
  */
-public class NoProductOfType extends Exception {
+public class NoProductOfTypeException extends Exception {
 
-    public NoProductOfType(){
+    public NoProductOfTypeException(){
         super("There is no products of that type at the storage");
     }
 }
