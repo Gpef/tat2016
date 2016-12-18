eff1e51860adfbe9f9ce75f8a28534090e275ccf
olegb <oleg.baslak@gmail.com>
2016-12-18 21:38:47 +0300

    Automated script now opens generated report after all

--- projects/wdautotest/auto/unix_run.sh
+++ projects/wdautotest/auto/unix_run.sh
@@ -1,28 +1,16 @@
 cd vagrant
-echo -----------------------------------------------------
-echo [TEST-INFO] Installing vagrant vm. Please, stand by.
-echo -----------------------------------------------------
 vagrant destroy --force
 vagrant up
-echo -----------------------------------------------------
-echo [TEST-INFO] VM is now running. Start verifying tests.
-echo -----------------------------------------------------
-cd ..
-cd ..
-mvn clean
+cd ../..
 rm -rf reports/
 mkdir reports
-mvn verify -Dwebdriver.driver=chrome
-mv target/report reports/chrome
+
+mvn clean verify -Dwebdriver.driver=chrome
+mv target\report reports\chrome
+cd reports\chrome\
+index.html
+cd ../..
+
 mvn clean
-mvn verify -Dwebdriver.driver=edge
-mv target/report reports/edge
-mvn clean
-mvn verify -Dwebdriver.driver=firefox
-mv target/report reports/firefox
-mvn clean
-echo -----------------------------------------------------
-echo [TEST-INFO] Tests passed. Reports placed in /reports.
-echo -----------------------------------------------------
 cd auto/vagrant
 vagrant destroy
--- projects/wdautotest/auto/win_run.cmd
+++ projects/wdautotest/auto/win_run.cmd
@@ -1,30 +1,18 @@
 @echo off
-title Automated testing
+title Wordpress automated testing
 cd vagrant
-echo -----------------------------------------------------
-echo [TEST-INFO] Installing vagrant vm. Please, stand by.
-echo -----------------------------------------------------
-vagrant destroy --force
-vagrant up
-echo -----------------------------------------------------
-echo [TEST-INFO] VM is now running. Start verifying tests.
-echo -----------------------------------------------------
-cd..
-cd..
-cmd /c mvn clean
+@echo vagrant destroy --force
+@3cho vagrant up
+cd ../..
 rmdir /s /q reports
 mkdir reports
-cmd /c mvn verify -Dwebdriver.driver=chrome
+
+cmd /c mvn clean verify -Dwebdriver.driver=chrome
 move target\report reports\chrome
+cd reports\chrome\
+cmd /c index.html
+cd ../..
+
 cmd /c mvn clean
-cmd /c mvn verify -Dwebdriver.driver=edge
-move target\report reports\edge
-cmd /c mvn clean
-cmd /c mvn verify -Dwebdriver.driver=firefox
-move target\report reports\firefox
-cmd /c mvn clean
-echo -----------------------------------------------------
-echo [TEST-INFO] Tests passed. Reports placed in /reports.
-echo -----------------------------------------------------
 cd auto\vagrant
 vagrant destroy
